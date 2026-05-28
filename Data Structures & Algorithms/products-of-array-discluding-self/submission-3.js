48ad83e641f812ee6b8f1d438a032b9057a7504b
Abbas <82901334+quicksilverr@users.noreply.github.com>
2026-05-28 16:14:32 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length
+        const result = Array(n).fill(0)
+        let prefix = 1
+        for(let i=0 ;i < n; i++){
+            result[i] = prefix
+            prefix = prefix * nums[i]
+        }
+
+        let suffix = 1
+        for(let i=n-1; i >=0; i--){
+            result[i] = suffix * result[i]
+            suffix = suffix * nums[i]
+        }
+        return result
+    }
+}
